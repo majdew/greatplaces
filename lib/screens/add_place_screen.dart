8ff765e8fd189063095ba12955e9bca5cfcf45ec
majdew <majd1999ziad@gmail.com>
2020-08-14 17:16:55 +0300

    add place screen design

---
+++ lib/screens/add_place_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/image_input.dart';
+
+class AddPlaceScreen extends StatefulWidget {
+  static const routeName = "/add-place";
+  @override
+  _AddPlaceScreenState createState() => _AddPlaceScreenState();
+}
+
+class _AddPlaceScreenState extends State<AddPlaceScreen> {
+  final _titleController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add a New Place"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          // in expnaded child take all the height it's can get
+          // screen height - button height to gurntee that  button at end
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(labelText: "Title"),
+                      controller: _titleController,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          RaisedButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+            label: Text("Add Place"),
+            elevation: 0,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            color: Theme.of(context).accentColor,
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/image_input.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  @override
+  _ImageInputState createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  File _storedImage;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey,
+            ),
+          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  "No Image Taken",
+                  textAlign: TextAlign.center,
+                ),
+          alignment: Alignment.center,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: FlatButton.icon(
+            onPressed: () {},
+            label: Text(
+              "Take Picture",
+              textAlign: TextAlign.center,
+            ),
+            textColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.camera),
+          ),
+        ),
+      ],
+    );
+  }
+}
